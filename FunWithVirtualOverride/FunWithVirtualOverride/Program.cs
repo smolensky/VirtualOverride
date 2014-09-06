a74bbf6619bceb047153269d6f5fd3116b3e80f1
Bohdan <te.dark@mail.ru>
2014-09-06 20:42:11 +0300

    geting knowledge about virtual/override

---
+++ FunWithVirtualOverride/FunWithVirtualOverride/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FunWithVirtualOverride
+{
+    class Program
+    {
+        static void Main()
+        {
+            Alfa a = new Beta();
+            Beta b = new Beta();
+
+            Console.WriteLine(a.GetFirst());
+            Console.WriteLine(a.GetSecond());
+            Console.WriteLine(b.GetFirst());
+            Console.WriteLine(b.GetSecond());
+            
+            Console.ReadKey();
+
+        }
+    }
+
+    class Alfa
+    {
+        public bool GetFirst()
+        {
+            return true;
+        }
+
+        public virtual bool GetSecond()
+        {
+            return true;
+        }
+    }
+
+    class Beta : Alfa
+    {
+        public new bool GetFirst()
+        {
+            return false;
+        }
+
+        public override bool GetSecond()
+        {
+            return false;
+        }
+    }
+}
